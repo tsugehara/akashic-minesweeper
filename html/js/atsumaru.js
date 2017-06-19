6919979694387dbba9d1109d43928a5e3d662d03
Toyokazu Tsugehara <toyokazu_tsugehara@dwango.co.jp>
2017-06-19 21:21:57 +0900

    dumpは出来たけどリプレーが動かない、というところまで

--- html/js/atsumaru.js
+++ html/js/atsumaru.js
@@ -26,12 +26,36 @@ window.addEventListener("load", function() {
 				}
 			});
 		}
+		var driver = window.sandboxDeveloperProps.driver;
 		if (loadButton) {
 			loadButton.addEventListener("click", function() {
 				if (window.RPGAtsumaru) {
 					game.external.atsumaru.storage.loadPlaylog("1");
 				} else {
 					var dump = window.localStorage.getItem("1");
+					var playlog = JSON.parse(dump);
+					window.sandboxDeveloperProps.amflow._tickList = playlog.tickList;
+					window.sandboxDeveloperProps.amflow._startPoints = playlog.startPoints;
+					driver.stopGame();
+					driver.changeState({
+						driverConfiguration: {
+							executionMode: window.sandboxDeveloperProps.gdr.ExecutionMode.PASSIVE,
+							playToken: window.sandboxDeveloperProps.gdr.MemoryAmflowClient.TOKEN_PASSIVE
+						},
+						loopConfiguration: {
+							playbackRate: 1,  // 実行速度もリセットしておく
+							loopMode: window.sandboxDeveloperProps.gdr.LoopMode.Replay,
+							delayIgnoreThreshold: Number.MAX_VALUE,  // Ugh! GameLoopがデフォルト値にリセットする方法を提供するべき
+							jumpTryThreshold: Number.MAX_VALUE
+						}
+					}, function (err) {
+						if (err) {
+							console.log(err);
+							return;
+						}
+						// driver.setNextAge(props.game.age);
+						driver.startGame();
+					});
 				}
 			});
 		}
